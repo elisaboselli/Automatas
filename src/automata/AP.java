96e4a78264e5f418461da70dc4315eab0c5be60b
Elisa Boselli <elisaboselli@hotmail.com>
2017-05-21 14:47:37 -0300

    Function from_dot

--- src/automata/AP.java
+++ src/automata/AP.java
@@ -3,17 +3,17 @@ package automata;
 import java.util.Iterator;
 import java.util.Set;
 import java.util.Stack;
+import java.lang.StringBuffer;
+import java.util.regex;
 
 import utils.Quintuple;
-
-
 
 public abstract class AP {
 
 	public static final Character Lambda = '_';
 	public static final Character Joker = '@';
 	public static final Character Initial = 'Z';
-	
+
 	protected State initial;
     protected Character stackInitial;
     protected Stack<Character> stack; //stack of the automaton
@@ -22,34 +22,101 @@ public abstract class AP {
     protected Set<Character> stackAlphabet; //Alphabet of the stack
     protected Set<Quintuple<State,Character,Character,String,State>> transitions; //delta function
     protected Set<State> finalStates;
-	
-	
+
+
     /*
      * A static constructor should be implemented depending on the final design of the automaton
      * */
-    
-    
+
+
     public static State getElemFromSet(Set<State> q,State o){
 		//TODO this method have to be implemented.
     	return null;
      }
-          
+
     public Set<State> final_states(){
         return finalStates;
     }
-     
+
     public State initial_state(){
         return initial;
     }
-    
+
     public Set<Character> alphabet(){
         return alphabet;
     }
-    
+
     public Set<State> states(){
         return states;
     }
-     
+
+    public DAFPila from_dot(String fileName){
+      BufferedReader br = new BufferedReader(new FileReader(fileName + ".txt"));
+      try {
+        String line = br.readLine();
+
+        while (line != null) {
+					String [] aux;
+					String nt;
+					Character tc, tp;
+					State from, to;
+					Quintuple<State,Character,Character,String,State> auxTransition;
+
+					Pattern initialState = Pattern.compile("^(inic->).*");
+					Matcher matInitalState = initialState.matcher(line);
+
+					Pattern finalState = Pattern.compile("^.*(\[).*(doublecircle).*");
+					Matcher matFinalState = finalState.matcher(line);
+
+					Pattern transition = Pattern.compile("^(q).*(->).*");
+					Matcher matTransition = transition.matcher(line);
+
+     			if(matInitalState.matches()){
+						aux = line.split("inic-> | ;");
+						this.initial.add(new State(aux[0].trim()));
+						this.states.add(this.initial);
+					}
+
+					if(matFinalState.matches()){
+						aux = line.split("\[");
+						this.finalStates.add(new State(aux[0].trim()));
+						this.states.add(new State(aux[0].trim()));
+					}
+
+					if(matTransition.matches()){
+						aux = line.split("-> | \s |  \" | /");
+
+						from = new State(aux[0].trim());
+						to = new State(aux[1].trim());
+						tc = aux[3].trim();
+						tp = aux[4].trim();
+						nt = aux[5].trim();
+
+						this.states.add(from);
+						this.states.add(to);
+
+						auxTransition = new Quintuple(from,tc,tp,nt,to);
+						this.transitions.add(auxTransition);
+
+					}
+
+					line = br.readLine();
+				}
+
+        } finally {
+          br.close();
+        }
+			/*digraph{
+inic[shape=point];
+inic->q0;
+q0->q1 [label="a/@/a"];
+q1->q1 [label="a/a/aa"];
+q1->q2 [label="b/a/_"];
+q2->q2 [label="b/b/_"];
+q2[shape=doublecircle];
+}*/
+		}
+
     public final String to_dot(){
         //assert rep_ok();
         char quotes= '"';
@@ -71,13 +138,13 @@ public abstract class AP {
         aux = aux + "}";
         return aux;
     }
-    
+
     /**
-    * this methods should be implemented in DFAPila 
+    * this methods should be implemented in DFAPila
     */
     public abstract boolean accepts(String string);
-     
+
     public abstract Object delta(State from, Character c);
-    
-    
+
+
 }
